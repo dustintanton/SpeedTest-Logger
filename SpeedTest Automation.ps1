985eb8b4fecbef442a2b31f00fbcd950e9c87d62
dustintanton <59024262+dustintanton@users.noreply.github.com>
2024-04-16 10:59:50 -0500

    Add files via upload

---
+++ SpeedTest Automation.ps1
@@ -0,0 +1,31 @@
+# Define the output file
+$outputFile = "C:\Users\Presto IT\Downloads\ookla-speedtest-1.2.0-win64\speedtest_log.csv"
+
+# Getting Timestamp variable
+$currentTimestamp = Get-Date -FOrmat "MM-dd-yyyy HH:mm:ss"
+
+# Check if the output file exists, and if not, create it with headers
+if (-not (Test-Path $outputFile)) {
+    "TimeStamp,Server Name,Server ID,Ping Latency,Ping Jitter,Packet Loss,Download Bandwidth,Upload Bandwidth,Download Bytes,Upload Bytes,Result,,,,,,,,,,,,Download Speed MB/s,Upload Speed MB/s,Formula is cell/1048576" | Out-File $outputFile
+}
+
+# Function to run speedtest and parse results
+function Perform-SpeedTest {
+    # Specify the full path to the speedtest executable
+    $speedtestExecutable = "C:\Users\Presto IT\Downloads\ookla-speedtest-1.2.0-win64\speedtest.exe"
+    
+    # Using the call operator (&) to execute the speedtest with full path
+    $result = & $speedtestExecutable -f csv
+    $result = $currentTimestamp + "," + $result.Trim()
+    Write-Host $result
+    $result | Out-File $outputFile -Append
+    Write-Host "Speed test performed and logged."
+}
+
+# Main loop to perform the speed test every hour
+Write-Host "Starting speed tests, press CTRL+C to stop."
+while ($true) {
+    Perform-SpeedTest
+    # Wait for 3600 seconds (1 hour) before the next test
+    Start-Sleep -Seconds 900
+}
